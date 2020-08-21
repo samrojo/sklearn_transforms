3d2b2971aac2df46a793ecf7cf89b8bf35978b08
samrojo <49177886+samrojo@users.noreply.github.com>
2020-08-21 01:10:51 -0500

    Update Transforms

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -12,5 +12,22 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+        
+        data["BACKEND_AVGxBEGGINER"] = data['AVG_SCORE_BACKEND']*data['NUM_COURSES_BEGINNER_BACKEND']
+        data["FRONTEND_AVGxBEGGINER"] = data['AVG_SCORE_FRONTEND']*data['NUM_COURSES_BEGINNER_FRONTEND']
+        #---
+        data["AVGBxDHOURS"] = data['AVG_SCORE_BACKEND']*data['HOURS_DATASCIENCE']
+        data["AVGBxDBEGGINER"] = data['AVG_SCORE_BACKEND']*data['NUM_COURSES_BEGINNER_DATASCIENCE']
+        data["AVGBxDADVANCED"] = data['AVG_SCORE_BACKEND']*data['NUM_COURSES_ADVANCED_DATASCIENCE']
+        
+        data["AVGBxFHOURS"] = data['AVG_SCORE_BACKEND']*data['HOURS_FRONTEND']
+        data["AVGBxFADVANCED"] = data['AVG_SCORE_BACKEND']*data['NUM_COURSES_ADVANCED_FRONTEND']
+        
+        data["AVGFxDHOURS"] = data['AVG_SCORE_BACKEND']*data['HOURS_DATASCIENCE']
+        
+        data["AVGFxBHOURS"] = data['AVG_SCORE_FRONTEND']*data['HOURS_BACKEND']
+        data["AVGFxBADVANCED"] = data['AVG_SCORE_FRONTEND']*data['NUM_COURSES_ADVANCED_BACKEND']
+        #---
+        
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
